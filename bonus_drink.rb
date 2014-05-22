4fd0471d0b0dda876c6673cff6d1f4093eeac416
techno-tanoC <techno.tanoc@gmail.com>
2014-05-23 01:08:05 +0900

    rec_calc

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,25 @@
 class BonusDrink
+  def self.rec_calc(total, mod)
+    bonus = mod / 3
+    part = mod % 3
+    total += mod - part
+
+    if bonus + part < 3
+      return total + bonus + part
+    end
+    rec_calc(total, bonus + part)
+  end
+
   def self.total_count_for(amount)
-    0
+    buff = amount
+    self.rec_calc(0, buff)
+  end
+
+  def self.answer
+    total_count_for(100)
+  end
+
+  def self.debug(amount)
+    puts total_count_for(amount)
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
+  specify { expect(BonusDrink.rec_calc(0, 0)).to eq 0 }
   specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+
+  specify { expect(BonusDrink.rec_calc(0, 3)).to eq 4 }
+  specify { expect(BonusDrink.total_count_for(3)).to eq 4 }
+
+  specify { expect(BonusDrink.rec_calc(0, 5)).to eq 7 }
+  specify { expect(BonusDrink.total_count_for(5)).to eq 7 }
+
+
+  specify { expect(BonusDrink.rec_calc(0, 11)).to eq 16 }
+  specify { expect(BonusDrink.total_count_for(11)).to eq 16 }
+
+  specify { expect(BonusDrink.answer).to be_a(Integer) }
 end
